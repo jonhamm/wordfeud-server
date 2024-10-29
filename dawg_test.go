a849859f75b36094c269fc2de1a5760c433b094f
Jon Hammerskov <jon@hammerskov.dk>
2024-10-29 07:25:42 +0100

    add verification of dawg generated words

--- dawg_test.go
+++ dawg_test.go
@@ -26,24 +26,43 @@ func Test_DawgBasic(t *testing.T) {
 	testDawgContent(t, language.Danish, content)
 }
 
+func Test_DawgComplete(t *testing.T) {
+	testDawgLanguage(t, language.Danish)
+}
+
 func MakeTestCorpusFromContent(language language.Tag, content []string) (*Corpus, error) {
 	data := strings.Join(content, "\n")
 	return NewCorpus(strings.NewReader(data), GetLanguageAlphabet(language))
 }
 
 func testDawgContent(t *testing.T, language language.Tag, corpusContent []string) {
-	var dawg *Dawg
 	corpus, err := MakeTestCorpusFromContent(language, corpusContent)
 	if err != nil {
 		t.Errorf("testDawgContent() failed to create corpus : %v", err)
 		return
 	}
-	dawg, err = MakeDawg(corpus)
+	testDawgCorpus(t, corpus)
+}
+
+func testDawgLanguage(t *testing.T, language language.Tag) {
+	corpus, err := GetFileCorpus(GetLanguageFileName(language), GetLanguageAlphabet((language)))
+	if err != nil {
+		t.Errorf("testDawgLanguage() failed to create corpus : %v", err)
+		return
+	}
+	testDawgCorpus(t, corpus)
+}
+
+func testDawgCorpus(t *testing.T, corpus *Corpus) {
+	dawg, err := MakeDawg(corpus)
 	if err != nil {
 		t.Errorf("testDawgContent() failed to create dawg : %v", err)
 		return
 	}
+	testDawg(t, dawg)
+}
 
+func testDawg(t *testing.T, dawg *Dawg) {
 	fmt.Print("\n\n")
 	verifyDawgCoverage(t, dawg)
 }
@@ -70,27 +89,34 @@ func verifyCorpusMatches(t *testing.T, dawg *Dawg) {
 }
 
 func verifyMatchesInCorpus(t *testing.T, dawg *Dawg) {
-	verifyMatchesInCorpusRecurse(t, dawg, dawg.initialState)
+	corpus := dawg.corpus
+	count := verifyMatchesInCorpusRecurse(t, dawg, dawg.initialState)
+	if count != corpus.wordCount {
+		t.Errorf("dawg generated %v words which is not equal to corpus wordcount of %v", count, corpus.wordCount)
+	}
 }
 
-func verifyMatchesInCorpusRecurse(t *testing.T, dawg *Dawg, state State) {
+func verifyMatchesInCorpusRecurse(t *testing.T, dawg *Dawg, state State) int {
+	count := 0
+	corpus := dawg.corpus
 	node := state.LastNode()
 	if node == nil {
-		return
+		return count
 	}
 	vertex := state.LastVertex()
 	if vertex != nil {
 		if vertex.final {
-			_, found := dawg.corpus.FindWord(state.word)
+			_, found := corpus.FindWord(state.word)
 			if !found {
-				t.Errorf("dawg genrated word \"%s\" not found in corpus", state.word.String(dawg.corpus))
-				return
+				t.Errorf("dawg genrated word \"%s\" not found in corpus", state.word.String(corpus))
+				return count
 			}
-
+			count++
 		}
 	}
 	for _, v := range node.vertices {
 		vs := dawg.Transition(state, v.letter)
-		verifyMatchesInCorpusRecurse(t, dawg, vs)
+		count += verifyMatchesInCorpusRecurse(t, dawg, vs)
 	}
+	return count
 }
